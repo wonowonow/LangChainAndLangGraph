bb46040b9fbcfaf2d144ffb18a618dedce23c3d1
wonowonow <123870616+wonowonow@users.noreply.github.com>
2025-04-06 20:41:49 +0900

    feat(search): 도구 사용할지 말지 Agent 판단

--- LangChain/langchain_router.py
+++ LangChain/langchain_router.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-from LangChain.search_agent import search_wiht_tavily
+from LangChain.search_agent import search_wiht_tavily, search_with_agent
 
 router = APIRouter()
 
+class message(BaseModel):
+    message: str
+
 @router.post("/search")
-async def search(query: str):
+async def search(body: message):
     """
     Tavily 도구를 사용하여 검색합니다.
-    :param query: 검색할 쿼리
+    :body: 쿼리
     """
-    return {"message": "Search results", "data": search_wiht_tavily(query)}
+    return {"message": "Search results", "data": search_wiht_tavily(body.message)}
+
+@router.post("/search-agent")
+async def search(body: message):
+    """
+    Agent 가 Tavily 도구를 사용할지 말지 구분하여 응답합니다.
+    :body: 쿼리
+    """
+    return {"message": "Search results", "data": search_with_agent(body.message)}
--- LangChain/search_agent.py
+++ LangChain/search_agent.py
@@ -1,6 +1,22 @@
 from langchain_community.tools import TavilySearchResults
+from langchain_openai import ChatOpenAI
 
 web_search = TavilySearchResults(max_results=5)
 
+llm = ChatOpenAI(model="gpt-4o-mini")
+llm_with_tools = llm.bind_tools(tools = [web_search])
+
 def search_wiht_tavily(query: str) -> list[str]:
     return web_search.invoke(query)
+
+def search_with_agent(query: str) -> list[str]:
+    """
+    LLM 이 Tavily 도구를 사용할지 말지 구분하며 응답합니다.
+    :param query: 쿼리
+    """
+    
+    ai_msg = llm_with_tools.invoke(query)
+    
+    tool_call = ai_msg.tool_calls[0]
+    
+    return web_search.invoke(tool_call["args"])
